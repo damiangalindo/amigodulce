9a9a15c2e3678aa52f0df9f8c7f2cbdd87a0bc3c
Damian Galindo Meza <demian007@gmail.com>
2014-09-09 20:58:33 -0500

    fff

--- app.rb
+++ app.rb
@@ -2,15 +2,13 @@ require 'rubygems'
 require './scrabbler'
 require 'sinatra'
 
-class App < Sinatra::Base
-  get "/:token/:name" do
-	  encrypted = File.read("result.txt").chomp
-	  data = Scrabbler.decrypt(encrypted)
-	  name = params[:name].gsub(/_/, " ").upcase
-	  if data.key?(params[:token]) && data[params[:token]].keys.map(&:upcase).include?(name)
-	    data[params[:token ]].inspect
-	  else
-	    "Nadie me quiere...todos me odian... voy a comerme un gusaniitoo :("
-	  end
-	end
+get "/:token/:name" do
+  encrypted = File.read("result.txt").chomp
+  data = Scrabbler.decrypt(encrypted)
+  name = params[:name].gsub(/_/, " ").upcase
+  if data.key?(params[:token]) && data[params[:token]].keys.map(&:upcase).include?(name)
+    data[params[:token ]].inspect
+  else
+    "Nadie me quiere...todos me odian... voy a comerme un gusaniitoo :("
+  end
 end
